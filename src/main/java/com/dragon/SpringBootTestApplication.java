44e715a943bbbfa8b6aa558bdeff8a582c5c6bcf
sonitanand91 <49591659+SonitAnand@users.noreply.github.com>
2020-04-26 15:46:45 +0530

    Update SpringBootTestApplication.java

--- src/main/java/com/dragon/SpringBootTestApplication.java
+++ src/main/java/com/dragon/SpringBootTestApplication.java
@@ -8,9 +8,6 @@ public class SpringBootTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTestApplication.class, args);
-		
-		adasdsadsad
-			asdsadsad
 	}
 
 }

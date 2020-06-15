168d2107c7a63c269e80e8bc8e4b56aad1e1ba5c
Sonit Anand <sonitanand@gmail.com>
2020-06-15 16:09:42 +0530

    updated security changes

--- src/main/java/com/dragon/securityconfiguration/MyUserDetailsService.java
+++ src/main/java/com/dragon/securityconfiguration/MyUserDetailsService.java
@@ -1,18 +1,20 @@
 package com.dragon.securityconfiguration;
 
 import com.dragon.repository.UserRepository;
-import com.dragon.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-    @Override
+    /*@Override
     public UserPrincipal loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findByUsername(username);
@@ -20,5 +22,9 @@ public class MyUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User not found");
 
         return new UserPrincipal(user);
+    }*/
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("sonit","sonit", new ArrayList<>());
     }
 }
